7ce9e056cbad73f344f509d8cc26ea2ae63c035c
felixilefz <117941565+felixilefz@users.noreply.github.com>
2022-11-24 14:43:19 -0500

    Add files via upload

---
+++ Elephant.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+
+public class Elephant extends Actor
+{
+
+    public void act() 
+    {
+       
+       MouseInfo m = Greenfoot.getMouseInfo();
+       System.out.println(m);
+       
+       if (m == null || (getX() == m.getX() && getY() == m.getY())) {
+           System.out.println(0);
+           return;
+       }
+       
+       turnTowards(m.getX(), m.getY());
+       move(1);
+    }    
+}
