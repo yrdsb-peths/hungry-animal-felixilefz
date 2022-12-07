9ba9f33a54a295d38086fb873bac48d9730a2c75
felixilefz <117941565+felixilefz@users.noreply.github.com>
2022-12-07 14:19:35 -0500

    tweak elephant behaviour

--- Elephant.java
+++ Elephant.java
@@ -5,7 +5,7 @@ public class Elephant extends Actor
 {
     private double speed = 0;
     private int speedCap = 8;
-    private MyWorld world;
+    private SimpleTimer animationTimer = new SimpleTimer(); 
     private GreenfootImage[] idleRight = new GreenfootImage[8];
     private GreenfootImage[] idleLeft = new GreenfootImage[8];
     private int animationIndex = 0;
@@ -14,10 +14,12 @@ public class Elephant extends Actor
     public Elephant() {
         for (int i = 0; i < idleRight.length; i++) {
             idleRight[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+            idleRight[i].scale(50, 50);
         }
         for (int i = 0; i < idleLeft.length; i++) {
             idleLeft[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
             idleLeft[i].mirrorHorizontally();
+            idleLeft[i].scale(50, 50);
         }
         setImage(idleRight[0]);
         animationState = "right";
@@ -27,7 +29,7 @@ public class Elephant extends Actor
     public void act() 
     {
        // kind of makes it slippery
-       world = (MyWorld) getWorld();
+       MyWorld world = (MyWorld) getWorld();
        if (Math.abs(speed) < speedCap) {
            
            if (Greenfoot.isKeyDown("d")) {
@@ -51,11 +53,16 @@ public class Elephant extends Actor
            animationState = "right";
        }
        move((int)speed);
+       // if the elephant moves too far offscreen it will cancel the movement
+       if (getX() < -30 || getX() > world.getWidth()+30) {
+           move((int) speed * -1);
+       }
        eat();
        animate();
     }    
     
     public void eat() {
+        MyWorld world = (MyWorld) getWorld();
         if (isTouching(Apple.class)) {
             removeTouching(Apple.class);
             world.caughtApple();
@@ -65,14 +72,17 @@ public class Elephant extends Actor
     }
     
     public void animate() {
-        if (animationState == "right") {
-            animationIndex = (animationIndex + 1) % idleRight.length;
-            setImage(idleRight[animationIndex]);
-            animationIndex ++;
-        } else if (animationState == "left") {
-            animationIndex = (animationIndex + 1) % idleLeft.length;
-            setImage(idleLeft[animationIndex]);
-            animationIndex ++; 
+        if (animationTimer.millisElapsed() > 100) {
+            if (animationState == "right") {
+                animationIndex = (animationIndex + 1) % idleRight.length;
+                setImage(idleRight[animationIndex]);
+                animationIndex ++;
+            } else if (animationState == "left") {
+                animationIndex = (animationIndex + 1) % idleLeft.length;
+                setImage(idleLeft[animationIndex]);
+                animationIndex ++; 
+            }
+            animationTimer.mark();
         }
         
     }
